00bc4f378709aedbc31df707a8e12c33a44c8042
Saki <zha_zha@outlook.com>
2015-11-29 00:41:14 +0800

    tv index

---
+++ Application/Tv/Controller/IndexController.class.php
@@ -0,0 +1,22 @@
+<?php
+namespace Tv\Controller;
+use Think\Controller;
+
+
+class IndexController extends Controller {
+
+
+    public function index(){
+        layout(false);
+        $this->display();
+
+
+
+
+
+
+    }
+
+
+
+}
